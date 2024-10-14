b84ca8c033a7fb456bbac66070d65b40a6d3e49d
Samuel TM <111026713+OLeDouxEt@users.noreply.github.com>
2024-10-13 19:39:57 -0500

    Initial early build to just control brightness

---
+++ DataHelper.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace LogiWiz
+{
+    public class DataHelper
+    {
+        public static string IP = "172.20.1.42";
+        public static int Port = 38899;
+
+        public static string ResolveInput(int input, int mode)
+        {
+            // Need to check the bulbs current state before sending a new setting.
+            string getCurrState = "{\"method\":\"getPilot\",\"params\":{}}";
+            string currState = SendData(getCurrState);
+            if (currState == null) {
+                Console.WriteLine("Unable to communicate with Bulb");
+                return "1";
+            }
+            //
+            Dictionary<string, string> stateMap = new Dictionary<string, string>()
+            {
+                {"temp", "0"},
+                {"dimming", "0"}
+            };
+            string[] resArray = currState.Split(',');
+            foreach (string s in resArray)
+            {
+                if (s.Contains("temp"))
+                {
+                    string[] tempArr = s.Split(":");
+                    stateMap["temp"] = tempArr[1];
+                }else if (s.Contains("dimming"))
+                {
+                    string[] dimArr = s.Split(":");
+                    stateMap["dimming"] = dimArr[1].Trim('}');
+                }
+            }
+            
+            switch (mode)
+            {
+                // Case for dimming or brightening lights
+                case 0:
+                    string newDimState = PrepParams(input, stateMap["dimming"]);
+                    string warmthState = stateMap["temp"];
+                    string newParams = $"{{\"method\": \"setPilot\",\"params\": {{\"temp\": {warmthState}, \"dimming\": {newDimState}}}}}";
+                    Console.WriteLine(newParams);
+                    string lightResponse = SendData(newParams);
+                    Console.WriteLine(lightResponse);
+                    break;
+                // Case for warming or cooling lights
+                case 1:
+                    PrepParams(1, stateMap["temp"]);
+                    break;
+                // Case for turning lights on or off
+                case 2:
+                    break;
+            }
+            return currState;
+        }
+
+        private static string PrepParams(int input, string currState)
+        {
+            int newState = 0;
+            int intState = int.Parse(currState);
+            switch (input)
+            {
+                case 0:
+                    if (intState <= 90)
+                    {
+                        newState = intState + 10;
+                    }
+                    else
+                    {
+                        newState = 100;
+                    }
+                    break;
+                case 1:
+                    if (intState >= 20)
+                    {
+                        newState = intState - 10;
+                    }
+                    else
+                    {
+                        newState = 10;
+                    }
+                    break;
+            }
+            string newStringState = $"{newState}";
+            return newStringState;
+        }
+
+        private static string SendData(string reqParams)
+        {
+            UdpClient udpClient = new UdpClient();
+            IPAddress BulbIP = IPAddress.Parse(IP);
+            IPEndPoint Endpoint = new IPEndPoint(BulbIP, Port);
+            udpClient.Connect(BulbIP, Port);
+            udpClient.Client.ReceiveTimeout = 3000;
+            udpClient.Client.SendTimeout = 3000;
+
+            byte[] data = Encoding.ASCII.GetBytes(reqParams);
+            try
+            {
+                int sent = udpClient.Send(data, data.Length);
+                Console.WriteLine(sent);
+                byte[] rawRes = udpClient.Receive(ref Endpoint);
+                string response = System.Text.Encoding.ASCII.GetString(rawRes, 0, rawRes.Length);
+                return response;
+            }
+            catch (Exception e)
+            {
+                string response = e.ToString();
+                return response;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+
+namespace LogiWiz
+{
+
+    class LogiWizMain
+    {
+        static void Main(string[] args)
+        {
+            // Initialize the SDK
+            if (LogitechGSDK.LogiLcdInit("G15 Applet", LogitechGSDK.LOGI_LCD_TYPE_MONO | LogitechGSDK.LOGI_LCD_TYPE_COLOR))
+            {
+                Console.WriteLine("Logitech SDK initialized successfully.");
+                // Default mode is brightness labeled by 0
+                int CurrMode = 0;
+                while (true)
+                {
+                    bool Shutdown = false;
+                    // sleep is dynamically set if a button is pressed so the display will pause and the user
+                    // can see the displayed message. 
+                    int sleep = 0;
+                    bool btn0 = LogitechGSDK.LogiLcdIsButtonPressed(LogitechGSDK.LOGI_LCD_MONO_BUTTON_0);
+                    bool btn1 = LogitechGSDK.LogiLcdIsButtonPressed(LogitechGSDK.LOGI_LCD_MONO_BUTTON_1);
+                    bool btn2 = LogitechGSDK.LogiLcdIsButtonPressed(LogitechGSDK.LOGI_LCD_MONO_BUTTON_2);
+                    bool btn3 = LogitechGSDK.LogiLcdIsButtonPressed(LogitechGSDK.LOGI_LCD_MONO_BUTTON_3);
+                    if (btn0)
+                    {
+                        sleep = 1000;
+                        string modeDisplay = DisplayCurrMode(CurrMode);
+                        string dataSent = DataHelper.ResolveInput(0, CurrMode);
+                        LogitechGSDK.LogiLcdMonoSetText(0, $" LogiWiz Mode: {modeDisplay}");
+                        LogitechGSDK.LogiLcdMonoSetText(1, " Increasing");
+                        LogitechGSDK.LogiLcdMonoSetText(2, "");
+                        LogitechGSDK.LogiLcdMonoSetText(3, "     +            -          Mode      Exit");
+                    }
+                    else if (btn1)
+                    {
+                        sleep = 1000;
+                        string modeDisplay = DisplayCurrMode(CurrMode);
+                        string dataSent = DataHelper.ResolveInput(1, CurrMode);
+                        LogitechGSDK.LogiLcdMonoSetText(0, $" LogiWiz Mode: {modeDisplay}");
+                        LogitechGSDK.LogiLcdMonoSetText(1, " Decreasing");
+                        LogitechGSDK.LogiLcdMonoSetText(2, "");
+                        LogitechGSDK.LogiLcdMonoSetText(3, "     +            -          Mode      Exit");
+                    }
+                    else if (btn2)
+                    {
+                        CurrMode = IncrementMode(CurrMode);
+                        string modeDisplay = DisplayCurrMode(CurrMode);
+                        sleep = 1000;
+                        LogitechGSDK.LogiLcdMonoSetText(0, $" LogiWiz Mode: {modeDisplay}");
+                        LogitechGSDK.LogiLcdMonoSetText(1, " Changing Mode");
+                        LogitechGSDK.LogiLcdMonoSetText(2, "");
+                        LogitechGSDK.LogiLcdMonoSetText(3, "     +            -          Mode      Exit");
+                    }
+                    else if (btn3)
+                    {
+                        Shutdown = true;
+                        sleep = 1000;
+                        LogitechGSDK.LogiLcdMonoSetText(0, " LogiWiz Mode: ");
+                        LogitechGSDK.LogiLcdMonoSetText(1, " Shutting Down");
+                        LogitechGSDK.LogiLcdMonoSetText(2, "");
+                        LogitechGSDK.LogiLcdMonoSetText(3, "     +            -          Mode      Exit");
+                    }
+                    // Normal display loop at rest when no button is pressed
+                    else
+                    {
+                        string modeDisplay = DisplayCurrMode(CurrMode);
+                        LogitechGSDK.LogiLcdMonoSetText(0, $" LogiWiz Mode: {modeDisplay}");
+                        LogitechGSDK.LogiLcdMonoSetText(1, "");
+                        LogitechGSDK.LogiLcdMonoSetText(2, "");
+                        LogitechGSDK.LogiLcdMonoSetText(3, "     +            -          Mode      Exit");
+                    }
+
+                    // Update the LCD
+                    LogitechGSDK.LogiLcdUpdate();
+                    Thread.Sleep(sleep);
+                    if (Shutdown)
+                    {
+                        LogitechGSDK.LogiLcdShutdown();
+                        Environment.Exit(0);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Failed to initialize Logitech SDK.");
+            }
+        }
+
+        public static int IncrementMode(int mode)
+        {
+            int newMode;
+            if(mode < 2)
+            {
+                newMode = mode += 1;
+            }
+            else
+            {
+                newMode = 0;
+            }
+            return newMode;
+        }
+
+        public static string DisplayCurrMode(int mode)
+        {
+            string ModeToDisplay = "";
+            switch (mode)
+            {
+                case 0:
+                    ModeToDisplay = "Brightness";
+                    break;
+                case 1:
+                    ModeToDisplay = "Temperature";
+                    break;
+                case 2:
+                    ModeToDisplay = "On/Off";
+                    break;
+            }
+            return ModeToDisplay;
+        }
+    }
+}
