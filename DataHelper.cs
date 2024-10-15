812e137be474515c11982a437fff24fe73a677ec
OLeDouxEt <samtemartin6@outlook.com>
2024-10-15 00:02:14 -0500

    Network Error Handling and Warmth Settings Added
    Added error handling when unable to communicate with a bulb. Also added the ability to adjust warmth setting. "ResolveInput" function now returns the status of a user's input action to the main program for it to display.

--- DataHelper.cs
+++ DataHelper.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net;
+using System.Net.NetworkInformation;
 using System.Net.Sockets;
 using System.Text;
 
@@ -9,19 +9,33 @@ namespace LogiWiz
 {
     public class DataHelper
     {
-        public static string IP = "172.20.1.42";
+        public static string IP = "";
         public static int Port = 38899;
 
         public static string ResolveInput(int input, int mode)
         {
+            // Variable will be set depending on users input, if the input action was successfully, and will
+            // be returned to be displayed to inform the user.
+            string actionStatus = "";
+
+            // THIS WILL IMMEDIATELY return an error to display if a connection to the bulb cannot be
+            // established to avoid any further errors.
+            bool connectionStatus = TestConnection(IP);
+            if(connectionStatus == false)
+            {
+                actionStatus = "Cannot Communicate With Bulb";
+                return actionStatus;
+            }
+
             // Need to check the bulbs current state before sending a new setting.
             string getCurrState = "{\"method\":\"getPilot\",\"params\":{}}";
             string currState = SendData(getCurrState);
-            if (currState == null) {
+            if (currState == "null") {
                 Console.WriteLine("Unable to communicate with Bulb");
-                return "1";
+                actionStatus = "Cannot Communicate With Bulb";
+                return actionStatus;
             }
-            //
+            // Need to save the bulb's current setting as only one will change. Others will be passed to the new params sent.
             Dictionary<string, string> stateMap = new Dictionary<string, string>()
             {
                 {"temp", "0"},
@@ -41,57 +55,87 @@ namespace LogiWiz
                 }
             }
             
+            // Need to set dynamic limits and modifiers for temp settings and brightness settings as the range
+            // for brightness is 10-100 while temp is 1000-6200.
+            int upper = 100;
+            int lower = 10;
+            int modifier = 10;
+            string newParams;
+            string lightResponse;
             switch (mode)
             {
                 // Case for dimming or brightening lights
                 case 0:
-                    string newDimState = PrepParams(input, stateMap["dimming"]);
+                    string newDimState = PrepBasicParams(input, stateMap["dimming"], upper, lower, modifier);
+                    // Passing in the current state of temp setting fetched from
                     string warmthState = stateMap["temp"];
-                    string newParams = $"{{\"method\": \"setPilot\",\"params\": {{\"temp\": {warmthState}, \"dimming\": {newDimState}}}}}";
-                    Console.WriteLine(newParams);
-                    string lightResponse = SendData(newParams);
+                    newParams = $"{{\"method\": \"setPilot\",\"params\": {{\"temp\": {warmthState}, \"dimming\": {newDimState}}}}}";
+                    lightResponse = SendData(newParams);
                     Console.WriteLine(lightResponse);
+                    actionStatus = $"Setting Brightness To {newDimState}";
                     break;
                 // Case for warming or cooling lights
                 case 1:
-                    PrepParams(1, stateMap["temp"]);
+                    upper = 6200;
+                    lower = 1000;
+                    modifier = 1000;
+                    string newWarmState = PrepBasicParams(input, stateMap["temp"], upper, lower, modifier);
+                    string dimState = stateMap["dimming"];
+                    newParams = $"{{\"method\": \"setPilot\",\"params\": {{\"temp\": {newWarmState}, \"dimming\": {dimState}}}}}";
+                    lightResponse = SendData(newParams);
+                    Console.WriteLine(lightResponse);
+                    actionStatus = $"Setting Temperature To {newWarmState}";
                     break;
                 // Case for turning lights on or off
                 case 2:
                     break;
             }
-            return currState;
+            return actionStatus;
         }
 
-        private static string PrepParams(int input, string currState)
+        // Function to handle changing temperature, brightness, and power state. For brightness and temperature, upper and lower limits
+        // are passed in to set the ranges.
+        private static string PrepBasicParams(int input, string currState, int up, int low, int mod)
         {
             int newState = 0;
             int intState = int.Parse(currState);
             switch (input)
             {
                 case 0:
-                    if (intState <= 90)
+                    if (intState <= (up - mod))
                     {
-                        newState = intState + 10;
+                        newState = intState + mod;
                     }
                     else
                     {
-                        newState = 100;
+                        newState = up;
                     }
                     break;
                 case 1:
-                    if (intState >= 20)
+                    if (intState >= (low + mod))
                     {
-                        newState = intState - 10;
+                        newState = intState - mod;
                     }
                     else
                     {
-                        newState = 10;
+                        newState = low;
                     }
                     break;
             }
             string newStringState = $"{newState}";
             return newStringState;
+        }
+
+        public static bool TestConnection(string endpoint)
+        {
+            bool ConnectionGood = true;
+            Ping netTest = new Ping();
+            PingReply pingRes = netTest.Send(endpoint, 1000);
+            if (pingRes.Status.ToString() == "TimedOut")
+            {
+                ConnectionGood = false;
+            }
+            return ConnectionGood;
         }
 
         private static string SendData(string reqParams)
@@ -100,8 +144,8 @@ namespace LogiWiz
             IPAddress BulbIP = IPAddress.Parse(IP);
             IPEndPoint Endpoint = new IPEndPoint(BulbIP, Port);
             udpClient.Connect(BulbIP, Port);
-            udpClient.Client.ReceiveTimeout = 3000;
-            udpClient.Client.SendTimeout = 3000;
+            udpClient.Client.ReceiveTimeout = 2000;
+            udpClient.Client.SendTimeout = 2000;
 
             byte[] data = Encoding.ASCII.GetBytes(reqParams);
             try
@@ -115,8 +159,14 @@ namespace LogiWiz
             catch (Exception e)
             {
                 string response = e.ToString();
-                return response;
+                Console.WriteLine(response);
+                return "null";
             }
         }
+        /*
+        private static string SendAndReceive()
+        {
+
+        }*/
     }
 }
