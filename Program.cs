436acbe952fb2cf46651ee6e852149abd7187adb
Samuel TM <111026713+OLeDouxEt@users.noreply.github.com>
2024-10-16 23:38:16 -0500

    Display change for On/Off Mode
    Added function to handle input display generation depending on the mode selected. Also fixed applet name.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿namespace LogiWiz
+﻿using System;
+using System.Threading;
+
+namespace LogiWiz
 {
 
     class LogiWizMain
@@ -6,7 +9,7 @@
         static void Main(string[] args)
         {
             // Initialize the SDK
-            if (LogitechGSDK.LogiLcdInit("G15 Applet", LogitechGSDK.LOGI_LCD_TYPE_MONO | LogitechGSDK.LOGI_LCD_TYPE_COLOR))
+            if (LogitechGSDK.LogiLcdInit("LogiWiz", LogitechGSDK.LOGI_LCD_TYPE_MONO | LogitechGSDK.LOGI_LCD_TYPE_COLOR))
             {
                 Console.WriteLine("Logitech SDK initialized successfully.");
                 // Default mode is brightness labeled by 0
@@ -26,48 +29,53 @@
                         sleep = 1000;
                         string modeDisplay = DisplayCurrMode(CurrMode);
                         string dataSent = DataHelper.ResolveInput(0, CurrMode);
+                        string inputDisplay = DetermineInputDisplay(CurrMode);
                         LogitechGSDK.LogiLcdMonoSetText(0, $" LogiWiz Mode: {modeDisplay}");
                         LogitechGSDK.LogiLcdMonoSetText(1, $" {dataSent}");
                         LogitechGSDK.LogiLcdMonoSetText(2, "");
-                        LogitechGSDK.LogiLcdMonoSetText(3, "     +            -          Mode      Exit");
+                        LogitechGSDK.LogiLcdMonoSetText(3, $"{inputDisplay}Mode      Exit");
                     }
                     else if (btn1)
                     {
                         sleep = 1000;
                         string modeDisplay = DisplayCurrMode(CurrMode);
                         string dataSent = DataHelper.ResolveInput(1, CurrMode);
+                        string inputDisplay = DetermineInputDisplay(CurrMode);
                         LogitechGSDK.LogiLcdMonoSetText(0, $" LogiWiz Mode: {modeDisplay}");
                         LogitechGSDK.LogiLcdMonoSetText(1, $" {dataSent}");
                         LogitechGSDK.LogiLcdMonoSetText(2, "");
-                        LogitechGSDK.LogiLcdMonoSetText(3, "     +            -          Mode      Exit");
+                        LogitechGSDK.LogiLcdMonoSetText(3, $"{inputDisplay}Mode      Exit");
                     }
                     else if (btn2)
                     {
+                        sleep = 1000;
                         CurrMode = IncrementMode(CurrMode);
                         string modeDisplay = DisplayCurrMode(CurrMode);
-                        sleep = 1000;
+                        string inputDisplay = DetermineInputDisplay(CurrMode);
                         LogitechGSDK.LogiLcdMonoSetText(0, $" LogiWiz Mode: {modeDisplay}");
                         LogitechGSDK.LogiLcdMonoSetText(1, " Changing Mode");
                         LogitechGSDK.LogiLcdMonoSetText(2, "");
-                        LogitechGSDK.LogiLcdMonoSetText(3, "     +            -          Mode      Exit");
+                        LogitechGSDK.LogiLcdMonoSetText(3, $"{inputDisplay}Mode      Exit");
                     }
                     else if (btn3)
                     {
-                        Shutdown = true;
                         sleep = 1000;
+                        Shutdown = true;
+                        string inputDisplay = DetermineInputDisplay(CurrMode);
                         LogitechGSDK.LogiLcdMonoSetText(0, " LogiWiz Mode: ");
                         LogitechGSDK.LogiLcdMonoSetText(1, " Shutting Down");
                         LogitechGSDK.LogiLcdMonoSetText(2, "");
-                        LogitechGSDK.LogiLcdMonoSetText(3, "     +            -          Mode      Exit");
+                        LogitechGSDK.LogiLcdMonoSetText(3, $"{inputDisplay}Mode      Exit");
                     }
                     // Normal display loop at rest when no button is pressed
                     else
                     {
                         string modeDisplay = DisplayCurrMode(CurrMode);
+                        string inputDisplay = DetermineInputDisplay(CurrMode);
                         LogitechGSDK.LogiLcdMonoSetText(0, $" LogiWiz Mode: {modeDisplay}");
                         LogitechGSDK.LogiLcdMonoSetText(1, "");
                         LogitechGSDK.LogiLcdMonoSetText(2, "");
-                        LogitechGSDK.LogiLcdMonoSetText(3, "     +            -          Mode      Exit");
+                        LogitechGSDK.LogiLcdMonoSetText(3, $"{inputDisplay}Mode      Exit");
                     }
 
                     // Update the LCD
@@ -116,6 +124,25 @@
                     break;
             }
             return ModeToDisplay;
+        }
+        // Function to change inputs depending on currrent mode. Inputs to display will adjust whitespace depending
+        // on which inputs need to be displayed.
+        public static string DetermineInputDisplay(int mode)
+        {
+            string InputToDisplay = "";
+            switch (mode)
+            {
+                case 0:
+                    InputToDisplay = "     +            -          ";
+                    break;
+                case 1:
+                    InputToDisplay = "     +            -          ";
+                    break;
+                case 2:
+                    InputToDisplay = "   Off         On        ";
+                    break;
+            }
+            return InputToDisplay;
         }
     }
 }
