80fff11a5b9629ead83d43b8d555e798d437ce45
Samuel TM <111026713+OLeDouxEt@users.noreply.github.com>
2024-10-28 20:40:10 -0500

    Console Window Now Hides
    Created class and function to hide the console window.

---
+++ HideWindow.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace LogiWiz
+{
+    class HideWindow
+    {
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        const int SW_HIDE = 0;
+
+        public static void Hide()
+        {
+            // Hide the console window
+            var handle = GetConsoleWindow();
+            ShowWindow(handle, SW_HIDE);
+        }
+    }
+}
